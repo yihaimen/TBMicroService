b8960fb089dfae526c1b618f8f17f4849497c701
yihaimen <yihaimen@outlook.com>
2020-12-09 23:16:10 +0800

    feat: update webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const webpack = require('webpack'); // 访问内置的插件
 const path = require('path');
 
@@ -18,7 +19,7 @@ module.exports = {
     // 转换某些类型的模块的源代码
     rules: [
       // {test: /\.ts$/, use: ''}
-      { test: /\.less$/, use: [
+      { test: /\.less$/i, use: [
         { loader: 'style-loader' },
         {
           loader: 'css-loader',
@@ -30,7 +31,15 @@ module.exports = {
         { loader: 'less-loader' }
       ]},
       {
-        test: /\.(js|jsx)$/,
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(js|jsx)$/i,
         use: 'babel-loader'
       },
       { test: /\.ts$/, use: 'ts-loader' }
@@ -38,7 +47,8 @@ module.exports = {
   },
   // 打包优化，资源管理，注入环境变量
   plugins: [
+    new CleanWebpackPlugin(),
     new webpack.ProgressPlugin(),
-    new HtmlWebpackPlugin({template: './public/index.html'})
+    new HtmlWebpackPlugin({template: './public/index.html', title: 'TBMicroServices'})
   ]
 };
