141e86272b20ee29ad5b78766a2704732f2e33b6
yihaimen <yihaimen@outlook.com>
2020-12-10 21:00:57 +0800

    chore: update webpack config

---
+++ src/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(<h1>Hello React</h1>, document.getElementById('root'));
+
+
+// if (module.hot) {
+//   module.hot.accept('./print.js', function() {
+//     console.log('Accepting the updated printMe module!');
+//     printMe();
+//   })
+// }
+
+// if ('serviceWorker' in navigator) {
+//   window.addEventListener('load', () => {
+//     navigator.serviceWorker.register('/service-worker.js').then(registration => {
+//       console.log('SW registered: ', registration);
+//     }).catch(registrationError => {
+//       console.log('SW registration failed: ', registrationError);
+//     });
+//   });
+// }
--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,46 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const WorkboxPlugin = require('workbox-webpack-plugin');
+// const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 const webpack = require('webpack'); // 访问内置的插件
 const path = require('path');
 
-module.exports = {
-  entry: {
-    main: './src/index.tsx'
-    // vendor: '' // 第三方库
-  },
+module.exports = env => ({
+  entry: ['@babel/polyfill', './src/index.tsx'],
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: '[name].bundle.js' // '[name].[contenthash].bundle.js'
-    // publicPath // 可配置CDN
+    filename: '[name].bundle.js', // '[name].[contenthash].bundle.js'
+    publicPath: '/' // 可配置CDN
   },
   target: 'web',
-  mode: 'development', // development  production
+  mode: env.production ? 'production' : 'development',
+  devtool: env.production ? 'source-map' : 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+    hot: true,
+    host: 'localhost',
+    port: 8081
+  },
+  optimization: {
+    moduleIds: 'deterministic',
+    splitChunks: {
+      chunks: 'all',
+    },
+    runtimeChunk: 'single',
+    usedExports: true,
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'all',
+        },
+      },
+    }
+  },
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.jsx', '.js', '.json' ],
+  },
   module: {
     // 转换某些类型的模块的源代码
     rules: [
@@ -42,13 +68,20 @@ module.exports = {
         test: /\.(js|jsx)$/i,
         use: 'babel-loader'
       },
-      { test: /\.ts$/, use: 'ts-loader' }
+      { test: /\.ts|tsx$/, use: 'ts-loader' }
     ]
   },
   // 打包优化，资源管理，注入环境变量
   plugins: [
     new CleanWebpackPlugin(),
     new webpack.ProgressPlugin(),
-    new HtmlWebpackPlugin({template: './public/index.html', title: 'TBMicroServices'})
+    // new BundleAnalyzerPlugin(),
+    new HtmlWebpackPlugin({template: './public/index.html', title: 'TBMicroServices'}),
+    new WorkboxPlugin.GenerateSW({
+      // 这些选项帮助快速启用 ServiceWorkers
+      // 不允许遗留任何“旧的” ServiceWorkers
+      clientsClaim: true,
+      skipWaiting: true,
+    })
   ]
-};
+});
