87eb8f14493333f3bb46792c5b4adbb6eb4a6c57
yihaimen <yihaimen@outlook.com>
2020-12-11 22:39:45 +0800

    chore:update webpack config

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<h1>Hello React</h1>, document.getElementById('root'));
+ReactDOM.render(
+  <div>
+    <h1>Hello React!!</h1>
+  </div>,
+  document.getElementById('root'),
+);
 
+declare let module: any;
+if (module.hot) {
+  module.hot.accept();
+}
 
-// if (module.hot) {
-//   module.hot.accept('./print.js', function() {
-//     console.log('Accepting the updated printMe module!');
-//     printMe();
-//   })
-// }
-
-// if ('serviceWorker' in navigator) {
-//   window.addEventListener('load', () => {
-//     navigator.serviceWorker.register('/service-worker.js').then(registration => {
-//       console.log('SW registered: ', registration);
-//     }).catch(registrationError => {
-//       console.log('SW registration failed: ', registrationError);
-//     });
-//   });
-// }
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker
+      .register('/service-worker.js')
+      .then((registration) => {
+        console.log('SW registered: ', registration);
+      })
+      .catch((registrationError) => {
+        console.log('SW registration failed: ', registrationError);
+      });
+  });
+}
--- webpack.config.js
+++ webpack.config.js
@@ -5,57 +5,83 @@ const WorkboxPlugin = require('workbox-webpack-plugin');
 const webpack = require('webpack'); // 访问内置的插件
 const path = require('path');
 
-module.exports = env => ({
+module.exports = (env) => ({
   entry: ['@babel/polyfill', './src/index.tsx'],
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].bundle.js', // '[name].[contenthash].bundle.js'
-    publicPath: '/' // 可配置CDN
+    publicPath: '/', // 可配置CDN
   },
   target: 'web',
   mode: env.production ? 'production' : 'development',
   devtool: env.production ? 'source-map' : 'inline-source-map',
   devServer: {
     contentBase: './dist',
-    hot: true,
+    compress: true,
+    watchContentBase: true,
     host: 'localhost',
-    port: 8081
+    port: 8081,
+    hot: true,
+    open: true,
+    progress: true,
+    inline: true,
+    clientLogLevel: 'none',
+    historyApiFallback: true,
+    noInfo: false,
+    overlay: {
+      warnings: true,
+      errors: true,
+    },
   },
   optimization: {
     moduleIds: 'deterministic',
-    splitChunks: {
-      chunks: 'all',
-    },
     runtimeChunk: 'single',
     usedExports: true,
     splitChunks: {
       cacheGroups: {
         vendor: {
           test: /[\\/]node_modules[\\/]/,
+          priority: -10,
           name: 'vendors',
           chunks: 'all',
+          minSize: 30000, // 模块大于30k会被抽离到公共模块
+          minChunks: 1, // 模块出现1次就会被抽离到公共模块
+          maxAsyncRequests: 5, // 异步模块，一次最多只能被加载5个
+          maxInitialRequests: 3, // 入口模块最多只能加载3个
+        },
+        default: {
+          minChunks: 2,
+          priority: -20,
+          reuseExistingChunk: true,
+          name: 'default',
         },
       },
-    }
+    },
   },
   resolve: {
-    extensions: [ '.tsx', '.ts', '.jsx', '.js', '.json' ],
+    extensions: ['.tsx', '.ts', '.jsx', '.js', '.json'],
+    alias: {
+      '@': path.resolve(__dirname, 'src/'),
+    },
   },
   module: {
     // 转换某些类型的模块的源代码
     rules: [
       // {test: /\.ts$/, use: ''}
-      { test: /\.less$/i, use: [
-        { loader: 'style-loader' },
-        {
-          loader: 'css-loader',
-          options: {
-            modules: true
-          }
-        },
-        { loader: 'postcss-loader'},
-        { loader: 'less-loader' }
-      ]},
+      {
+        test: /\.less$/i,
+        use: [
+          { loader: 'style-loader' },
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+            },
+          },
+          { loader: 'postcss-loader' },
+          { loader: 'less-loader' },
+        ],
+      },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
@@ -65,23 +91,23 @@ module.exports = env => ({
         type: 'asset/resource',
       },
       {
-        test: /\.(js|jsx)$/i,
-        use: 'babel-loader'
+        test: /\.(ts|tsx)$/i,
+        exclude: /node_modules/,
+        use: 'babel-loader',
       },
-      { test: /\.ts|tsx$/, use: 'ts-loader' }
-    ]
+    ],
   },
   // 打包优化，资源管理，注入环境变量
   plugins: [
     new CleanWebpackPlugin(),
     new webpack.ProgressPlugin(),
     // new BundleAnalyzerPlugin(),
-    new HtmlWebpackPlugin({template: './public/index.html', title: 'TBMicroServices'}),
+    new HtmlWebpackPlugin({ template: './public/index.html', title: 'TBMicroServices' }),
     new WorkboxPlugin.GenerateSW({
       // 这些选项帮助快速启用 ServiceWorkers
       // 不允许遗留任何“旧的” ServiceWorkers
       clientsClaim: true,
       skipWaiting: true,
-    })
-  ]
+    }),
+  ],
 });
